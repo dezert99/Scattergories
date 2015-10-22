e1196a648104fbf77a3a7cc6f47f64cd491c7808
dezert99 <bubbamart99@gmail.com>
2015-10-22 19:06:08 +0800

    WORKING
    Joining and creating lobbies working

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var Session = Parse.Object.extend("Session");
 var session = new Session();
 var Player= Parse.Object.extend("Player");
 var player = new Player();
+var creator;
 player.set("Name", prompt("Enter your username"));
 player.save(null, {
        success: function(object) {
@@ -17,13 +18,16 @@ player.save(null, {
 function joinLobby(){
   var ses = Parse.Object.extend("Session");
     var query = new Parse.Query(ses);
-    query.equalTo("sessionName", document.getElementById("lobby").value);
+    query.equalTo("Name", document.getElementById("lobby").value);
+    query.include("Creator");
     query.find({
     success: function(results) {
       console.log("Successfully retrieved " + results.length );
       // Do something with the returned Parse.Object values
-      session = document.getElementById("lobby").value;
-      console.log("session: "+session);
+      session = results[0];
+      addPlayer(session,player);
+      console.log("session joined: "+session.get("Name"));
+      console.log("The creator is: "+session.get("Creator").get("Name"));
     },
     error: function(error) {
       alert("Error: " + error.code + " " + error.message);
@@ -33,50 +37,38 @@ function joinLobby(){
 function createLobby() {
   var ses = Parse.Object.extend("Session");
   var query = new Parse.Query(ses);
-  var exists = false;
   query.equalTo("sessionName", document.getElementById("lobby").value);
   query.find({
     success: function(results) {
-      console.log("Successfully retrieved " + results.length);
-      if (results.length != 0) {
+      console.log("Successfully retrieved " + results.length );
+      // Do something with the returned Parse.Object values
+      //session = document.getElementById("lobby").value;
+      if(results.length !=0 ){
         alert("Session already exists");
         return;
       }
-      session.set("sessionName", document.getElementById("lobby").value);
-      var Players = Parse.Object.extend("Player");
-      var query = new Parse.Query(Players);
-      query.equalTo("Name", "zzz");
-      query.find({
-        success: function(results) {
-          var query2 = new Parse.Query(Players);
-          query2.get(results[0].id, {
-            success: function(k) {
-              console.log(k);
-              session.add("Players", k);
-            },
-            error: function(object, error) {
-              // The object was not retrieved successfully.
-              // error is a Parse.Error with an error code and message.
-            }
-          });
-        },
-        error: function(error) {
-          alert("Error: " + error.code + " " + error.message);
-        }
-      });
-      session.save(null, {
-        success: function(object) {
-          $(".success").show();
-        },
-        error: function(model, error) {
-          $(".error").show();
-        }
-      });
+      session.set("Creator", player);
+      session.set("Name", document.getElementById("lobby").value);
+      addPlayer(session, player);     
     },
     error: function(error) {
       alert("Error: " + error.code + " " + error.message);
     }
-  });
+  }); 
+}
+function addPlayer(session, player){
+      console.log("Creating session...");
+      session.add("Players", player);
+      session.save(null, {
+        success: function(object) {
+          $(".success").show();
+          console.log("Session created!");
+        },
+        error: function(model, error) {
+          $(".error").show();
+          console.log("Session failed to create. Error code: "+error.code);
+        }
+      });
 }
 function test(){
  var ses = Parse.Object.extend("Session");
